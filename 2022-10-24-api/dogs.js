5000f94cae0b934316b3a26dec40339a4e120141
RomanasV <romanasvenckus@gmail.com>
2022-10-25 13:10:56 +0300

    breeds object iteration added

--- 2022-10-24-api/dogs.js
+++ 2022-10-24-api/dogs.js
@@ -5,32 +5,43 @@ function init() {
       let allBreeds = data.message;
       console.log(allBreeds);
 
-      let breedKeys = Object.keys(allBreeds);
-      let breedValues = Object.values(allBreeds);
-      let breedEntries = Object.entries(allBreeds);
+      // let breedKeys = Object.keys(allBreeds);
+      // let breedValues = Object.values(allBreeds);
+      // let breedEntries = Object.entries(allBreeds);
 
-      console.log(breedKeys);
-      console.log(breedValues);
-      console.log(breedEntries);
+      // console.log(breedKeys);
+      // console.log(breedValues);
+      // console.log(breedEntries);
 
-      breedKeys.map(key => {
-        console.log(key);
-      })
+      // breedKeys.map(key => {
+      //   console.log(key);
+      // })
       
-      breedValues.map(value => {
-        console.log(value);
-      })
+      // breedValues.map(value => {
+      //   console.log(value);
+      // })
 
-      breedKeys.map((key, index) => {
-        console.log(key);
-        console.log(breedValues[index]);
-      })
+      // breedKeys.map((key, index) => {
+      //   console.log(key);
+      //   console.log(breedValues[index]);
+      // })
 
       // breedEntries.map(entry => {
       //   console.log(entry);
       //   console.log(entry[0]);
       //   console.log(entry[1]);
       // })
+
+      for (key in allBreeds) {
+        let mainBreed = key;
+        let subBreeds = allBreeds[key];
+
+        let optionElement = document.createElement('option');
+        optionElement.textContent = mainBreed;
+        optionElement.value = mainBreed;
+        console.log(optionElement);
+
+      }
     })
 }
 
