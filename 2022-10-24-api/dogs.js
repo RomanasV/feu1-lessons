9ae347092b7a2d1f47a86309626b7a1855b34912
RomanasV <romanasvenckus@gmail.com>
2022-10-25 12:02:19 +0300

    dogs api task started

---
+++ 2022-10-24-api/dogs.js
@@ -0,0 +1,37 @@
+function init() {
+  fetch('https://dog.ceo/api/breeds/list/all')
+    .then(res => res.json())
+    .then(data => {
+      let allBreeds = data.message;
+      console.log(allBreeds);
+
+      let breedKeys = Object.keys(allBreeds);
+      let breedValues = Object.values(allBreeds);
+      let breedEntries = Object.entries(allBreeds);
+
+      console.log(breedKeys);
+      console.log(breedValues);
+      console.log(breedEntries);
+
+      breedKeys.map(key => {
+        console.log(key);
+      })
+      
+      breedValues.map(value => {
+        console.log(value);
+      })
+
+      breedKeys.map((key, index) => {
+        console.log(key);
+        console.log(breedValues[index]);
+      })
+
+      // breedEntries.map(entry => {
+      //   console.log(entry);
+      //   console.log(entry[0]);
+      //   console.log(entry[1]);
+      // })
+    })
+}
+
+init();
